d5d105dbb60399890f3ec613bf8e4cb83ee612fe
dragon <newlong@126.com>
2012-11-18 23:39:39 +0800

    增加mysql数据库操作类库

--- test_mysql.lisp
+++ test_mysql.lisp
@@ -1,2 +1,13 @@
-#!sbcl --script
+#!/usr/local/sbcl/bin/sbcl --script
 
+(defvar *lisp-dirs* "~/workspace/stock/" "project root directory")
+(require 'asdf)
+(setf asdf:*central-registry* 
+      '(*default-pathname-defaults*
+	(concatenate 'string "~/workspace/stock/" "uffi/")
+	(concatenate 'string "~/workspace/stock/" "rt/")
+	(concatenate 'string *lisp-dirs* "clsql/")))
+
+
+
+(asdf:oos 'asdf:test-op 'clsql)
